f2fda053662e5521910f659d6bf2b88d432d8830
CosmicVeil <mohan.dixit2008@gmail.com>
2025-06-28 17:39:51 -0400

    add ML

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/mohandixit/Documents/Programming /Sports Project/venv"
+set -gx VIRTUAL_ENV "/Users/mohandixit/Documents/GitHub/Sports-Website/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
